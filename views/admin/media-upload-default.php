9a43dd2a01d4592cbd4746bd6fcb24cdbb3cb0c1
loumray <loumray@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-26 23:32:12 +0000

    Removed no more needed files for release 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/remote-medias-lite/trunk@997434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/media-upload-default.php
+++
@@ -1,7 +0,0 @@
-<?php
-?>
-<script type="text/html" id="tmpl-remote-media-upload">
-  <div>
-  <p><?php _e('Upgrade with an extension plugin to unlock amazing feature like direct upload','remote-medias-lite'); ?></p>
-  </div>
-</script>
